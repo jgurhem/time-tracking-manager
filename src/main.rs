49ced50c5b2d8614809aa5e99de6120ecc5dd0ba
Jérôme Gurhem <jgurhem@aneo.fr>
2024-08-20 19:18:22 +0200

    fix: remove block_on() from main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+use std::error::Error;
 use clap::Parser;
-use futures::executor::block_on;
 use time_tracking_manager::{args::Args, filters::{predicate_filter, FilterParam}, providers::{clockify::Clockify, Provider}};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     dbg!(&args);
 
     let mut c = Clockify::new(args.token.as_str());
-    let entries = c.load(args.start, args.end);
-
-    let entries = block_on(entries).unwrap();
+    let entries = c.load(args.start, args.end).await?;
 
     let param = FilterParam::build(&args);
     let entries = entries.into_iter().filter(|x| { predicate_filter(&x, &param) });
 
-    for e in entries{
+    for e in entries {
         println!("entry {:?}", e);
     }
+
+    Ok(())
 }
