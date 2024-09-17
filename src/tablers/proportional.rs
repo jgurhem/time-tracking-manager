84a991d3501f49046b46f035a9d852fb10b8bbe1
Jérôme Gurhem <jgurhem@aneo.fr>
2024-09-17 20:58:45 +0200

    fix: make sure days % sum to 100%

--- src/tablers/proportional.rs
+++ src/tablers/proportional.rs
@@ -1,20 +1,20 @@
-use std::{collections::HashMap, fmt::Debug, fmt::Display, marker::PhantomData};
+use std::collections::HashMap;
 
 use chrono::{DateTime, TimeDelta, Utc};
 
+use rand::prelude::Distribution;
+use rand::rngs::StdRng;
+use rand::{distributions::Uniform, SeedableRng};
+
 use super::{MyTable, Table, Tabler};
 
-pub struct Proportional<T> {
-    p: PhantomData<T>,
-}
+pub struct Proportional {}
 
 ///
 /// Compute table containing the daily sum for each entry related to same project and task divided by the sum of entries
 ///
-impl<'a, T: Display + Clone + Default + TryFrom<i64, Error: Debug> + 'a> Tabler<'a>
-    for Proportional<T>
-{
-    type Table = MyTable<T>
+impl<'a> Tabler<'a> for Proportional {
+    type Table = MyTable<u8>
     where
         Self: 'a;
 
@@ -51,12 +51,52 @@ impl<'a, T: Display + Clone + Default + TryFrom<i64, Error: Debug> + 'a> Tabler<
                     continue;
                 }
                 let v = 100 * x.num_seconds() / days.get(d).unwrap().num_seconds();
-                let v = T::try_from(v).unwrap();
+                let v = u8::try_from(v).unwrap();
                 table.insert(s.to_string(), *d, v);
             }
         }
 
-        // todo : ensure the values in each columns are equal to 100
+        // Compute sum per day
+        let mut days: HashMap<DateTime<Utc>, u8> = HashMap::new();
+
+        for s in table.row_headers() {
+            for d in table.col_headers() {
+                let v = table.get(s.to_string(), *d);
+
+                match days.insert(*d, v) {
+                    Some(old) => {
+                        let new = days.get_mut(&d).unwrap();
+                        *new += old;
+                    }
+                    None => {}
+                }
+            }
+        }
+
+        let mut rng = StdRng::seed_from_u64(1);
+
+        // Randomly adjust values so that total per day is 100
+        for d in days.clone().keys().into_iter() {
+            let n = *days.entry(*d).or_insert(100);
+            if n == 100 {
+                continue;
+            }
+
+            let mut rows = Vec::new();
+            for row in table.row_headers.clone().into_iter() {
+                if table.get(row.to_string(), *d) != 0 {
+                    rows.push(row.clone());
+                }
+            }
+
+            let dis = Uniform::new(0, rows.len());
+
+            for _ in 0..100 - n {
+                *table
+                    .get_mut(rows[dis.sample(&mut rng)].to_string(), *d)
+                    .unwrap() += 1;
+            }
+        }
 
         table
     }
