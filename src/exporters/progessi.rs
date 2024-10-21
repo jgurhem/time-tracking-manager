2d6d2457d717e272bbdb6b3d4d1e5d9588b7bdd1
Jérôme Gurhem <jerome.gurhem@hotmail.fr>
2024-10-21 08:21:21 +0200

    feat: add preview for progessi

--- src/exporters/progessi.rs
+++ src/exporters/progessi.rs
@@ -17,12 +17,14 @@ use crate::{
 use std::sync::Arc;
 use std::sync::Mutex;
 use web_sys::{
-    console::log_1, Document, Event, HtmlButtonElement, HtmlDivElement, HtmlInputElement,
-    HtmlOptionElement, HtmlSelectElement, NodeList,
+    console::log_1, Document, Event, HtmlButtonElement, HtmlDivElement, HtmlElement,
+    HtmlInputElement, HtmlLiElement, HtmlOptionElement, HtmlSelectElement, HtmlStyleElement,
+    HtmlUListElement, NodeList,
 };
 
 use super::Exporter;
 
+#[derive(Clone)]
 pub struct Progessi {
     pub start: DateTime<Utc>,
     pub document: Document,
@@ -292,6 +294,146 @@ impl Progessi {
     }
 }
 
+#[derive(Clone)]
+pub struct ProgessiPreview {
+    start: DateTime<Utc>,
+    document: Document,
+}
+
+impl ProgessiPreview {
+    pub fn new(start: DateTime<Utc>, document: Document) -> ProgessiPreview {
+        let head = document.head().unwrap();
+        let style = document
+            .create_element("style")
+            .unwrap()
+            .dyn_into::<HtmlStyleElement>()
+            .unwrap();
+
+        style.set_type("text/css");
+        style.set_text_content(Some(
+            r"
+            #ttm-preview {
+                display: table;
+                border-collapse: collapse;
+              }
+
+            #ttm-row {
+                display: table-row;
+            }
+
+            #ttm-cell {
+                display: table-cell;
+                padding: 5px;
+                border: 1px solid #ccc;
+                text-align: center;
+            }
+            ",
+        ));
+        head.append_child(&style).unwrap();
+
+        ProgessiPreview { start, document }
+    }
+}
+
+fn create_cell(document: &Document, text: &str) -> HtmlElement {
+    let cell = document
+        .create_element("div")
+        .unwrap()
+        .dyn_into::<HtmlDivElement>()
+        .unwrap();
+    cell.set_text_content(Some(text));
+    cell.set_id("ttm-cell");
+
+    cell.into()
+}
+
+fn create_row(document: &Document) -> HtmlElement {
+    let row = document
+        .create_element("li")
+        .unwrap()
+        .dyn_into::<HtmlLiElement>()
+        .unwrap();
+    row.set_id("ttm-row");
+
+    row.into()
+}
+
+impl<'a> Exporter<'a> for ProgessiPreview {
+    type Table = MyTable<u8>
+    where
+        Self: 'a;
+
+    fn export(
+        &mut self,
+        table: &Self::Table,
+        display: &HashMap<String, String>,
+    ) -> Result<(), Box<dyn Error>> {
+        if let Some(element) = self
+            .document
+            .query_selector("#ttm-preview")
+            .expect("old preview query was not valid")
+        {
+            element.remove();
+        }
+
+        let element = self
+            .document
+            .query_selector("#TIMESHEET_MESSAGE")
+            .expect("element in which to add preview was not found")
+            .expect("element in which to add preview was not found")
+            .dyn_into::<HtmlDivElement>()
+            .expect("should be a div element");
+
+        let preview = self
+            .document
+            .create_element("ul")
+            .unwrap()
+            .dyn_into::<HtmlUListElement>()
+            .unwrap();
+        preview.set_id("ttm-preview");
+        element.append_child(&preview).unwrap();
+
+        let row = create_row(&self.document);
+        preview.append_child(&row).unwrap();
+
+        let cell = create_cell(
+            &self.document,
+            &(self.start.year().to_string() + "/" + &self.start.month().to_string()),
+        );
+        row.append_child(&cell).unwrap();
+
+        let mut col_headers: Vec<DateTime<Utc>> = table.col_headers().cloned().collect();
+        col_headers.sort();
+
+        for date in &col_headers {
+            let cell = create_cell(&self.document, &date.day().to_string());
+            row.append_child(&cell).unwrap();
+        }
+
+        let row_headers: Vec<String> = table.row_headers().cloned().collect();
+
+        for r in row_headers {
+            let row = create_row(&self.document);
+            preview.append_child(&row).unwrap();
+            let cell = create_cell(
+                &self.document,
+                &display.get(&r).unwrap_or(&r).to_lowercase(),
+            );
+            row.append_child(&cell).unwrap();
+
+            for date in &col_headers {
+                let span = create_cell(
+                    &self.document,
+                    &table.get(r.clone(), date.clone()).to_string(),
+                );
+                row.append_child(&span).unwrap();
+            }
+        }
+
+        Ok(())
+    }
+}
+
 async fn download_entries(handle: Arc<Mutex<ProviderHandle>>) {
     handle
         .lock()
@@ -302,6 +444,18 @@ async fn download_entries(handle: Arc<Mutex<ProviderHandle>>) {
     handle.lock().unwrap().process().unwrap_throw();
 }
 
+fn create_button(document: &Document, text: &str) -> HtmlButtonElement {
+    let button = document
+        .create_element("button")
+        .unwrap()
+        .dyn_into::<HtmlButtonElement>()
+        .unwrap();
+    button.set_text_content(Some(text));
+    button.set_type("button");
+    button.set_class_name("btn btn-primary btn-sm");
+    button
+}
+
 #[wasm_bindgen]
 impl ProgessiHandle {
     pub async fn new(args: Args, document: JsValue) -> ProgessiHandle {
@@ -310,23 +464,9 @@ impl ProgessiHandle {
             .dyn_into::<Document>()
             .expect("input should be a document");
 
-        let dowload = document
-            .create_element("button")
-            .unwrap()
-            .dyn_into::<HtmlButtonElement>()
-            .unwrap();
-        dowload.set_text_content(Some("Refresh entries"));
-        dowload.set_type("button");
-        dowload.set_class_name("btn btn-primary btn-sm");
-
-        let fill = document
-            .create_element("button")
-            .unwrap()
-            .dyn_into::<HtmlButtonElement>()
-            .unwrap();
-        fill.set_text_content(Some("Fill time table"));
-        fill.set_type("button");
-        fill.set_class_name("btn btn-primary btn-sm");
+        let dowload = create_button(&document, "Refresh entries");
+        let fill = create_button(&document, "Fill time table");
+        let preview = create_button(&document, "Preview");
 
         let element = document
             .query_selector(".fc-addcontrol")
@@ -366,18 +506,27 @@ impl ProgessiHandle {
         on_click.forget();
 
         let clone = Arc::clone(&handle);
+        let progessi = Progessi {
+            start: start.clone(),
+            document: document.clone(),
+        };
         let on_click = EventListener::new(&fill, "click", move |_event| {
             let handle = clone.lock().unwrap();
-            let progessi = Progessi {
-                start: start.clone(),
-                document: document.clone(),
-            };
-            handle.export(Box::new(progessi)).unwrap_throw();
+            handle.export(Box::new(progessi.clone())).unwrap_throw();
+        });
+        on_click.forget();
+
+        let progessi = ProgessiPreview::new(start.clone(), document.clone());
+        let clone = Arc::clone(&handle);
+        let on_click = EventListener::new(&preview, "click", move |_event| {
+            let handle = clone.lock().unwrap();
+            handle.export(Box::new(progessi.clone())).unwrap_throw();
         });
         on_click.forget();
 
         element.append_child(&dowload).unwrap();
         element.append_child(&fill).unwrap();
+        element.append_child(&preview).unwrap();
 
         ProgessiHandle {}
     }
